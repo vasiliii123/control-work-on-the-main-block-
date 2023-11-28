1a8e0718141196415619f43590e35c9a22f2f9fc
unknown <vaseksalovik6789@gmail.com>
2023-11-28 20:21:45 +0300

    array-element

--- Program.cs
+++ Program.cs
@@ -13,3 +13,10 @@ int size  = int.Parse(Console.ReadLine());
 
 string[] arr1 = new string[size];
 
+for (int i = 0; i < size; i++)
+{
+	Console.Write("Введите элемент массива: ");
+    string result = Console.ReadLine();
+	arr1[i] = result;  
+}
+
